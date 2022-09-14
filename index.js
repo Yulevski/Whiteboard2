bf589a8127baf3b0efdf0132b9e4c86fcc04d220
Yulevski <yonaiyuu@gmail.com>
2022-09-14 19:54:55 +0900

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;//http://localhost:3000/
+
+app.use(express.static(__dirname + '/public'));
+
+function onConnection(socket){
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+
+}
+
+
+//io.on('connection', onConnection);
+io.on('connection', onConnection, (socket) => {
+ 
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+    console.log("send");
+  });
+  });
+  
+http.listen(port, () => console.log('listening on port ' + port));
